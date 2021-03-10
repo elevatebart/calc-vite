dcafe9a255d1b543a126bd325454e7a831e304f7
ElevateBart <ledouxb@gmail.com>
2021-03-10 17:14:15 -0600

    first commit

---
+++ src/components/Display.spec.tsx
@@ -0,0 +1,8 @@
+import { mount } from "@cypress/vue";
+import CalcDisplay from "./Display.vue";
+
+describe("<CalcDisplay />", () => {
+  it("should look like a display", () => {
+    mount(() => <CalcDisplay>8000</CalcDisplay>);
+  });
+});
