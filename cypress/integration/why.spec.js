b3ade86bd47a207f0c0f1485c09b452d8c559720
ElevateBart <ledouxb@gmail.com>
2021-04-21 15:59:26 -0500

    add test titke and project id

--- cypress/integration/why.spec.js
+++ cypress/integration/why.spec.js
@@ -1,6 +1,6 @@
 //this file ha to stay or else cypress will create a bunch of examples every time it starts...
 
-describe(() => {
+describe("why?", () => {
   it("why?", () => {
     expect(true).to.be.true;
   });
