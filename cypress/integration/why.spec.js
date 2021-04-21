e8476eace94f8907b3db139cb5d882c40937ef2b
ElevateBart <ledouxb@gmail.com>
2021-04-21 16:12:15 -0500

    remove useless empty test

--- cypress/integration/why.spec.js
+++ cypress/integration/why.spec.js
@@ -1,8 +1,2 @@
 // this file has to stay or else cypress
 // will create a bunch of examples every time it starts...
-
-describe("why?", () => {
-  it("why?", () => {
-    expect(true).to.be.true;
-  });
-});
