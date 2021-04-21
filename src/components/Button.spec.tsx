088022bdeca232b14cf08cbf85a19a15e9dbda50
ElevateBart <ledouxb@gmail.com>
2021-04-21 13:34:23 -0500

    screenshot buttons

--- src/components/Button.spec.tsx
+++ src/components/Button.spec.tsx
@@ -4,28 +4,32 @@ import CalcButton from "./Button.vue";
 describe("<CalcButton />", () => {
   it("should look like a button", () => {
     mount(() => <CalcButton value="5" />);
+    cy.screenshot();
   });
 
   it("should be orange when operator variant", () => {
     mount(() => <CalcButton variant="operator" value="+" />);
+    cy.screenshot();
   });
 
   it("should be darkened when dark variant", () => {
     mount(() => <CalcButton variant="dark" value="AC" />);
+    cy.screenshot();
   });
 
   it("should be wider when wide variant", () => {
     mount(CalcButton, {
-      props:{
+      props: {
         value: "0",
-        variant: "wide"
+        variant: "wide",
       },
-      data(){
-        return{
-          test: 7
-        }
-      }
+      data() {
+        return {
+          test: 7,
+        };
+      },
     });
+    cy.screenshot();
   });
 
   it("should emit a click when clicking on the button", () => {
