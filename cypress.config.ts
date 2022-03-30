8d9b1ac31be1d9d2e0fa1ee86c71ce515eff102f
ElevateBart <ledouxb@gmail.com>
2022-03-29 21:09:06 -0500

    update config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,16 @@
+import { defineConfig } from "cypress"
 import coverageTask from "@cypress/code-coverage/task"
-import { startDevServer } from "@cypress/vite-dev-server"
+import { devServer } from "@cypress/vite-dev-server"
 
-export default {
+export default defineConfig({
   fixturesFolder: false,
-  componentFolder: "src",
-  testFiles: "**/*.spec.{tsx,js}",
   component: {
-    testFiles: "**/*.spec.{tsx,js}",
-    async devServer(options) {
-      return startDevServer({ options })
-    },
+    specPattern: "src/**/*.spec.{tsx,js}",
+    devServer,
     setupNodeEvents(on, config){
       coverageTask(on, config)
 
       return config
     }
   }
-}
+})
