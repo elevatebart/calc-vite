7eace8604eb99896e716d87aaf989d798978ab62
elevatebart <bart@cypress.io>
2023-07-17 19:24:25 +0200

    add cy extensions

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   fixturesFolder: false,
   projectId: "z4jb27",
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
